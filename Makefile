e5bc13dadc5f67af59361d684cd3c14e9233bc64
Brandon Blanker Lim-it <flamendless8@gmail.com>
2018-12-02 08:55:46 +0800

    Changes

--- Makefile
+++ Makefile
@@ -2,8 +2,10 @@ PROJECT_NAME = MathProject
 BUILD = Release
 
 test:
-	cmake --build build/${BUILD} --target install
 	./bin/${BUILD}/${PROJECT_NAME}
+
+compile:
+	cmake --build build/${BUILD} --target install
 
 rebuild:
 	cmake -H. -Bbuild/${BUILD} -DCMAKE_BUILD_TYPE=${BUILD} -DCMAKE_INSTALL_PREFIX:PATH=`realpath bin/${BUILD}`
--- src/helpers.h
+++ src/helpers.h
@@ -1,7 +1,26 @@
 #pragma once
 
+#include "imgui.h"
+#include "menu.h"
+
 class Helpers
 {
 public:
 	static void ShowHelp(const char* desc);
 };
+
+void Helpers::ShowHelp(const char* desc)
+{
+	if (!Menu::isShowHelp)
+		return;
+
+	ImGui::TextDisabled("(?)");
+	if (ImGui::IsItemHovered())
+	{
+		ImGui::BeginTooltip();
+		ImGui::PushTextWrapPos(ImGui::GetFontSize() * 35.0f);
+		ImGui::TextUnformatted(desc);
+		ImGui::PopTextWrapPos();
+		ImGui::EndTooltip();
+	}
+}
